c72f62dadddfde667c0a34d3e4e179b7734d7dc0
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2022-07-13 10:55:20 +0700

    Add redis

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RailsDocker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.cache_store = :redis_store, {
+      host: "redis",
+      port: 6379,
+      db: 0,
+    }, {expires_in: 2.hours}
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
